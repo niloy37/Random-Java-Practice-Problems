52e76825a378c70c5a860b7d7a2bc49668e42ff4
Niloy Rahman <havocisgod@gmail.com>
2018-10-07 17:07:00 +0600

    Add files via upload

---
+++ PrePost.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+
+class PrePost {
+public static void main (String [] args){
+ int a=100;
+
+System.out.println(a++);
+
+int b=a ;
+ System.out.println(++b);
+
+
+}
+
+}
