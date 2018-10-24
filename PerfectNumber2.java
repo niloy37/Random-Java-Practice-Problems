fa6a473e4ca3154b1fe702360a16b9035a339547
Niloy Rahman <havocisgod@gmail.com>
2018-10-24 21:06:59 +0600

    Add files via upload

---
+++ PerfectNumber2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class PerfectNumber2{
+public static void main(String[] args){
+     
+    System.out.println("Enter number");
+    Scanner var = new Scanner(System.in);
+    int number=var.nextInt();
+
+    for (int i = 1;i <= number; i++) { 
+        int sum = 0;
+        for (int j = 1; j < i; j++) {
+            if ((i % j) == 0) {
+                sum += j;
+            } 
+        }  
+        if (sum == i){           
+            System.out.println(sum);
+        }          
+    }      
+} 
+}
