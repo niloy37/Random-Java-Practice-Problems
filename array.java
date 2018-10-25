386bf3ec73cd1e7854f522017fcd88b57f84ed2e
Niloy Rahman <havocisgod@gmail.com>
2018-10-25 08:42:50 +0600

    Add files via upload

---
+++ array.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class array {
+public static void main (String [] args){
+
+int [][] a = {{1,2,3},{4,5,6},{7,8,9}};
+int sum=0;
+
+for(int i=0;i<3;i++) {
+
+sum = sum+a[i][0];
+
+
+
+}
+
+System.out.println(sum);
+
+
+}
+
+
+
+
+
+}
