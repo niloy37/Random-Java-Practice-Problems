25faf868c749e30dd4b8f2ec0ffea303ea17255d
Niloy Rahman <havocisgod@gmail.com>
2018-10-27 20:46:48 +0600

    Determine OUTPUT

---
+++ mrh1MidSummer18/Mrh1Question.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Mrh1Question {
+	public static void main(String[] args) {
+		int x=9; int y=11;
+		if(x<10)
+			if(y>10)
+		System.out.println("****");
+			else 
+				System.out.println("$$$$");
+		System.out.println("#####");
+	}
+
+}
