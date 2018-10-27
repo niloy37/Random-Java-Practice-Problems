8626aa1b9edc7cba360c2dd689cc8733d78131b1
Niloy Rahman <havocisgod@gmail.com>
2018-10-27 20:55:40 +0600

    Conditional Operator True/False

---
+++ mrh1MidSummer18/MRH1JavaQuestion2.java
@@ -0,0 +1,14 @@
+
+public class MRH1JavaQuestion2 {
+public static void main(String[] args) {
+	int a=5 ;
+	int b=10;
+	int c=-6;
+	
+	System.out.println(a>b&&a<c);
+	System.out.println(b>15&&c<0 || a>0);
+	System.out.println(a==c || b>a);
+	System.out.println(a/2.0==0.0 && b/2.0!=0.0 || c<0.0);
+	System.out.println(a>>1<0);
+}
+}
