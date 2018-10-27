c8d3d2d975f83ea6c1ea648cf7f80b2b812b25e0
Niloy Rahman <havocisgod@gmail.com>
2018-10-27 21:36:51 +0600

    Update MRH1JavaQuestion2.java

--- mrh1MidSummer18/MRH1JavaQuestion2.java
+++ mrh1MidSummer18/MRH1JavaQuestion2.java
@@ -5,10 +5,10 @@ public static void main(String[] args) {
 	int b=10;
 	int c=-6;
 	
-	System.out.println(a>b&&a<c);
-	System.out.println(b>15&&c<0 || a>0);
-	System.out.println(a==c || b>a);
-	System.out.println(a/2.0==0.0 && b/2.0!=0.0 || c<0.0);
-	System.out.println(a>>1<0);
+	System.out.println(a>b&&a<c); //false
+	System.out.println(b>15&&c<0 || a>0); //true
+	System.out.println(a==c || b>a); //true
+	System.out.println(a/2.0==0.0 && b/2.0!=0.0 || c<0.0); //true
+	System.out.println(a>>1<0); //false
 }
 }
