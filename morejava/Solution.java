37613b6f783d9c0ef75f538b7c6c220aaa9daa85
Niloy Rahman <havocisgod@gmail.com>
2018-11-20 00:08:27 +0600

    uploaded for video

---
+++ morejava/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+public static void main(String [] args) {
+            sayHelloWorld();
+}
+public static void sayHelloWorld() {
+
+System.out.println("Hello,World");
+ }
+}
