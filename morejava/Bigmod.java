be64d2376ad72f7fbe035cef0867d30ff8c4efeb
Niloy Rahman <havocisgod@gmail.com>
2018-11-26 22:24:26 +0600

    Add files via upload

---
+++ morejava/Bigmod.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Bigmod {
+public static void main(String [] args) {
+	Scanner var = new Scanner(System.in);
+	
+	System.out.println("Enter Big Number");
+	String s = var.nextLine();
+	System.out.println("Enter number you want to mod with");
+	int a = var.nextInt();
+	System.out.println(bigmod(s,a));
+	
+	
+}
+
+
+
+
+public static int bigmod(String s , int a) {
+	int res=0;
+	for(int i=0;i<s.length();i++) {
+		res = (res*10+(s.charAt(i)-'0'))%a;
+	}
+	return res;
+	
+}
+
+}
