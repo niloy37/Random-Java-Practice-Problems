c15302a8f1f3aaf158030a6b28c3cff78af1eeeb
Niloy Rahman <havocisgod@gmail.com>
2018-11-27 22:45:13 +0600

    Constructor //chaining

---
+++ morejava/instanceo.java
@@ -0,0 +1,20 @@
+package morejava;
+
+ class instanceo {
+
+	 public static void main(String [] args) {
+		z q = new z();
+	 }
+}
+ class a{
+	 a() {
+		 System.out.println("Example of Consturctor chaining");
+	 }
+ }
+ class z extends a{
+	 z(){
+		 System.out.println("Hello World");
+		 
+	 }
+ }
+
