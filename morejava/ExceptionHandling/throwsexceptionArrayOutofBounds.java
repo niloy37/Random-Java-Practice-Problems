37a7ecbe6d80caff1b8e538ff5445262907eebd5
Niloy Rahman <havocisgod@gmail.com>
2018-12-02 16:33:10 +0600

    Create throwsexceptionArrayOutofBounds.java

---
+++ morejava/ExceptionHandling/throwsexceptionArrayOutofBounds.java
@@ -0,0 +1,21 @@
+package morejava;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Justincase {
+	public static void main (String [] args) throws java.lang.Exception
+	
+	{
+		int [] array = {1,2,3,4,5};
+		try {
+			for(int i=0;i<=array.length;i++) {
+				System.out.println(array[i]);
+			}
+		}catch(Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+
+
