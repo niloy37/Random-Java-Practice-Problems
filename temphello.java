184086c5aa3756eb438f7c1b4ca5357bfef73964
Niloy Rahman <niloy.rahman18@northsouth.edu>
2019-10-13 22:10:16 +0600

    tempted

--- temphello.java
+++ temphello.java
@@ -2,5 +2,6 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
+      //usefunc
    }
 }
